100701aa63942f30c3f4eb2833efd3ace9215a28
RenX <panrenx.global@gmail.com>
2026-05-03 08:41:40 +0800

    修改邮箱

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,17 +1,11 @@
 import { Resend } from 'resend'
 
+const resend = new Resend(process.env.RESEND_API_KEY)
+
 export async function sendWelcomeEmail(
   userEmail: string,
   userName: string
 ) {
-  const apiKey = process.env.RESEND_API_KEY
-
-  if (!apiKey) {
-    throw new Error('RESEND_API_KEY is not configured')
-  }
-
-  const resend = new Resend(apiKey)
-
   await resend.emails.send({
     from: '纸片人男友 <onboarding@resend.dev>',
     to: userEmail,
